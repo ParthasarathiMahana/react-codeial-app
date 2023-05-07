62eaab896aa0098f32d82a64b75ab3492dd2a310
Parthasarathi Mahana <parthasarathimahana@gmail.com>
2023-05-07 12:25:19 +0530

    api set up is done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,16 @@
+import { getPosts } from "../api";
+import {useEffect} from 'react';
+
 function App() {
+
+  useEffect(()=>{
+    const fetchPosts = async()=>{
+      const response = await getPosts();
+      console.log('response', response);
+    }
+    fetchPosts();
+  },[])
+
   return (
     <div>
       <h1>Hello World.</h1>
