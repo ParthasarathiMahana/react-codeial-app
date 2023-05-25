72f3c463a30abaf37423e769b2e532da6b9dae11
Parthasarathi Mahana <parthasarathimahana@gmail.com>
2023-05-25 07:30:59 +0530

    profile edit options with all responsive buttons added.

--- src/pages/Setting.js
+++ src/pages/Setting.js
@@ -1,8 +1,16 @@
 import styles from '../styles/settings.module.css';
+import { useState } from 'react';
 import  {useAuth} from '../hooks';
 
 const Setting = () =>{
     const auth = useAuth();
+    const [editMode, setEditMode] = useState(false);
+    const [name, setName] = useState(auth.user?.name ? auth.user.name:''); 
+    const [password, setPassword] = useState(''); 
+    const [confirmPassword, setConfirmPassword] = useState(''); 
+    const [savingForm, setSavingForm] = useState(false); 
+
+    const updateProfile=()=>{};
 
 
     return(
@@ -18,21 +26,32 @@ const Setting = () =>{
 
         <div className={styles.field}>
             <div className={styles.fieldName}>Name</div>
-            <div className={styles.fieldValue}>{auth.user?.name}</div>
+            {editMode?
+            <input type='text' value={name} onChange={(e)=>setName(e.target.value)}/>
+            :<div className={styles.fieldValue}>{auth.user?.name}</div>}
         </div>
 
+        {editMode && <>
         <div className={styles.field}>
             <div className={styles.fieldName}>Password</div>
-            <input type='password'/>
+            <input type='password' value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
         </div>
 
         <div className={styles.field}>
             <div className={styles.fieldName}>Confirm Password</div>
-            <input type='password'/>
+            <input type='password' value={confirmPassword} onChange={(e)=>{setConfirmPassword(e.target.value)}}/>
         </div>
+        </>}
 
         <div className={styles.btnGrp}>
-            <button className={`button ${styles.editBtn}`}>Edit Profile</button>
+            {editMode?<>
+                <button className={`button ${styles.editBtn}`} onClick={updateProfile }>
+                    {savingForm?'Saving From...':'Save From'}
+                </button>
+            <button className={`button ${styles.editBtn}`} onClick={(e)=>setEditMode(false)}>Go Back</button>
+            </>:
+            <button className={`button ${styles.editBtn}`} onClick={(e)=>setEditMode(true)}>Edit Profile</button>
+        }
         </div>
     </div>
     );
